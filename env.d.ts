283d70fac24dd531a94abb81f40145d0be189aea
Raye <astingraye@proton.me>
2025-04-22 21:16:59 +0100

    remove unused webpack module declaration

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="@moonlight-mod/types" />
 
-declare module "@moonlight-mod/wp/guildBeGone_rmGuild" {
-  export * from "guildBeGone/webpackModules/rmGuild";
-}
-
 declare module "@moonlight-mod/wp/channelEmojis_components" {
   export * from "channelEmojis/webpackModules/components";
 }
